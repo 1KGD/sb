160b388ebeab3662460eab18a66c3eb41ae1de8d
mrmcscreamypant <161328176+mrmcscreamypant@users.noreply.github.com>
2026-07-08 21:50:29 -0400

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,3 +1,10 @@
+from sphynx import __display_version__
+
 extensions = [
   "sphinx.ext.todo"
 ]
+
+project = "STARBLOOM"
+copyright = "2026-%Y John Schiefelebein"
+release = version = __display_version__
+show_authors = True
