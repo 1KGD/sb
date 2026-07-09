ef66c2dc29cfbfc88c5e2d71f0d7b3fac3052a66
mrmcscreamypant <161328176+mrmcscreamypant@users.noreply.github.com>
2026-07-08 22:31:11 -0400

    oops

--- docs/conf.py
+++ docs/conf.py
@@ -1,12 +1,9 @@
-from sphinx import __display_version__
-
 extensions = [
   "sphinx.ext.todo"
 ]
 
 project = "STARBLOOM"
 copyright = "2026-%Y John Schiefelebein"
-release = version = __display_version__
 show_authors = True
 
 html_theme = "bizstyle"
