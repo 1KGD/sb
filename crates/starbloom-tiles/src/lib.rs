972fde105f904b8fcfc9f653b27a5b92da06c07c
John Schiefelbein <161328176+mrmcscreamypant@users.noreply.github.com>
2026-09-08 15:10:36 +0000

    fix: Switch tile regestration logging to debug level

--- crates/starbloom-tiles/src/lib.rs
+++ crates/starbloom-tiles/src/lib.rs
@@ -34,7 +34,7 @@ impl TileRegestry {
         if self.ids.values().find(|v| **v == id).is_none() {
             self.ids.insert(self.ids.len() as TileRepr, id);
         }
-        log!(Level::Info, "Regestered tile '{}'", id);
+        debug!("Regestered tile '{}'", id);
     }
 
     pub fn get_tile_by_idx(&self, idx: &TileRepr) -> &Tile {
