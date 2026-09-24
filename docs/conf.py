2aab614a6394811fb9b24a682c7ff20023bb2f04
John Schiefelbein <161328176+mrmcscreamypant@users.noreply.github.com>
2026-09-24 16:50:26 -0400

    fix: go back to book theme

--- docs/conf.py
+++ docs/conf.py
@@ -18,7 +18,4 @@ show_authors = False
 
 new_tab_link_show_external_link_icon = True
 
-html_theme = "solar_theme"
-
-import solar_theme
-html_theme_path = [solar_theme.theme_path]
+html_theme = "sphinx_book_theme"
